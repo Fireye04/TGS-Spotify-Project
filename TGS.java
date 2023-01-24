49d13922d689ba3ca248a228ceb559b92d2e1139
Xenon <99437495+XenonBalance@users.noreply.github.com>
2023-01-24 20:51:39 +0000

    stuff

---
+++ TGS.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class TGS{
+//MAIN METHOD
+    public static void main(String[] args){
+        Playlist test = new Playlist("Artist", "Song", "Album");
+        System.out.println(test.getArtist());
+    }
+}
