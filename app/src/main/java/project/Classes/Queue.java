e8b4625917a39bca2dfb74e58b7dd65d28946278
Kai Koehler <codekai16@gmail.com>
2023-02-02 14:11:13 -0700

    feb 2

--- app/src/main/java/project/Classes/Queue.java
+++ app/src/main/java/project/Classes/Queue.java
@@ -1,4 +1,5 @@
 package project.Classes;
 
 public class Queue {
+    
 }
--- app/src/main/java/project/SpotifyApi/Builder.java
+++ app/src/main/java/project/SpotifyApi/Builder.java
@@ -2,33 +2,63 @@ package project.SpotifyApi;
 
 import se.michaelthelin.spotify.IHttpManager;
 
+import java.net.URI;
+
 public class Builder {
-    //The default authentication host of Spotify API calls.
-    public static final String DEFAULT_AUTHENTICATION_HOST = "accounts.spotify.com";
+    private IHttpManager httpManager;
+    private String scheme;
+    private String host;
+    private Integer port;
+    private String proxyUrl;
+    private Integer proxyPort;
+    private Integer proxyUsername;
+    private Integer proxyPassword;
+    private String clientId;
+    private String clientSecret;
+    private URI redirectUri;
+    private String accessToken;
+    private String refreshToken;
 
-    //The default authentication port of Spotify API calls.
-    public static final int DEFAULT_AUTHENTICATION_PORT = 443;
-
-    //The default authentication http scheme of Spotify API calls.
-    public static final String DEFAULT_AUTHENTICATION_STRING = "https";
-
-    //The default host of Spotify API calls.
-    public static final String DEFAULT_HOST = "api.spotify.com";
-
-    //An HttpManager configured with default settings.
-    public static final int DEFAULT_PORT = 443;
-
-    //The default http scheme of Spotify API calls.
-    public static final String DEFAULT_SCHEME = "https";
-
-    public static IHttpManager httpManager;
-
+    //Constructor
     public Builder(){
 
     }
-
-    void setHttpManager(IHttpManager httpManager){
+    //Setters
+    public void setHttpManager(IHttpManager httpManager){
         this.httpManager = httpManager;
     }
-
+    public void setScheme(String scheme){
+        this.scheme = scheme;
+    }
+    public void setHost(String host){
+        this.host = host;
+    }
+    public void setPort(Integer port){
+        this.port = port;
+    }
+    public void setProxyUrl(String proxyUrl){
+        this.proxyUrl = proxyUrl;
+    }
+    public void setProxyPort(Integer proxyPort){
+        this.proxyPort = proxyPort;
+    }
+    public void setProxyUsername(Integer proxyUsername){ this.proxyUsername = proxyUsername; }
+    public void setProxyPassword(Integer proxyPassword) {
+        this.proxyPassword = proxyPassword;
+    }
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+    public void setRedirectUri(URI redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
