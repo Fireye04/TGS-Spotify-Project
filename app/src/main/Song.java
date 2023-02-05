80bc949fde58f9e4a249484fd8a8f2a69f3b1d94
Goblnn <94708050+Goblnn@users.noreply.github.com>
2023-02-04 22:05:56 -0700

    make it look nice

--- app/src/main/Song.java
+++ app/src/main/Song.java
@@ -3,6 +3,7 @@ public class Song{
     private String artist;
     private String song;
     private String album;
+    
     public Song(String artist, String song, String album){
         this.artist = artist;
         this.song = song;
@@ -13,18 +14,23 @@ public class Song{
     public String getArtist() {
         return artist;
     }
+    
     public void setArtist(String artist) {
         this.artist = artist;
     }
+    
     public String getSong() {
         return song;
     }
+    
     public void setSong(String song) {
         this.song = song;
     }
+    
     public String getAlbum() {
         return album;
     }
+    
     public void setAlbum(String album) {
         this.album = album;
     }
